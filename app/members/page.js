ffad63024a2ce98a566a2db8fafea440079c1be9
akash2885 <apuzhakkal414@gmail.com>
2024-11-13 00:07:31 -0500

    Pictures of members

--- app/members/page.js
+++ app/members/page.js
@@ -34,7 +34,7 @@ const executiveMembers = [
     {
         name: "Akash",
         position: "VP of Tech",
-        image: "/images/Akash.jpeg",
+        image: "/images/Akash.jpg",
         year: "Senior",
         major: "Computer Science",
         linkedin: "https://www.linkedin.com/in/akash-puzhakkal/",
@@ -138,7 +138,7 @@ const MembersPage = () => {
 
     return (
         <div className="relative isolate min-h-screen bg-gray-900 text-white">
-            {/* Gradient Background */}
+            {/* Top Gradient Background */}
             <div
                 aria-hidden="true"
                 className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
@@ -149,6 +149,20 @@ const MembersPage = () => {
                             'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
                     }}
                     className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#3b82f6] to-[#1e40af] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                />
+            </div>
+
+            {/* Bottom-Right Gradient Background */}
+            <div
+                aria-hidden="true"
+                className="absolute inset-x-0 bottom-0 -z-10 transform-gpu overflow-hidden blur-3xl"
+            >
+                <div
+                    style={{
+                        clipPath:
+                            'polygon(0% 0%, 27.5% 0%, 45.2% 65.5%, 47.5% 41.7%, 52.4% 31.9%, 60.2% 37.6%, 72.5% 67.5%, 80.7% 98%, 85.5% 99.9%, 97.5% 73.1%, 100% 38.4%, 74.1% 55.9%, 76.1% 2.3%, 27.6% 23.2%, 17.9% 0%, 0.1% 35.1%)',
+                    }}
+                    className="relative right-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#3b82f6] to-[#1e40af] opacity-30 sm:right-[calc(50%-30rem)] sm:w-[72.1875rem]"
                 />
             </div>
 

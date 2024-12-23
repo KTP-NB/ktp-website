9cd57935837ebc1480846cc6134764c301adf422
akash2885 <apuzhakkal414@gmail.com>
2024-12-23 02:27:58 -0500

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,19 @@ module.exports = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        chart: {
+          1: 'hsl(var(--chart-1))',
+          2: 'hsl(var(--chart-2))',
+          3: 'hsl(var(--chart-3))',
+          4: 'hsl(var(--chart-4))',
+          5: 'hsl(var(--chart-5))',
+        },
       },
     },
   },
   plugins: [],
+  darkMode: 'class',
 };
